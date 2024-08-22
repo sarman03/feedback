1bea5cd82377c79ffbee73d162016d1e5b5a7e0d
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-08-22 12:32:07 +0530

    resend

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,21 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs"
+
+export async function POST(request:Request){
+    await dbConnect()
+
+    try {
+        const {username, email, password} = await request.json()
+    } catch (error) {
+        console.error("Error registering user", error)
+        return Response.json({
+            success:false,
+            message: "Error registering user"
+        },
+    {
+        status:500
+    })
+    }
+}
